0d889272b4fae465fcdd5ed980d0e0754f377d95
Aleks Tamarkin <aibolit@live.coom>
2017-03-03 19:02:55 -0500

    .

--- public_html/BaseInvaders.js
+++ public_html/BaseInvaders.js
@@ -143,7 +143,7 @@ $(document).ready(function () {
     }
 
     var init = true;
-    var ws = new WebSocket("ws://localhost:17427");
+    var ws = new WebSocket("ws://codebb.cloudapp.net:17427");
     ws.onopen = function (evt) {
         console.log(evt);
     };
