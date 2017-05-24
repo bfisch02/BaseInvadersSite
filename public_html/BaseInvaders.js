8fd980ca7cd9e5f7126b2f27521d31f30c6b593b
bfisch02 <brett.fischler@gmail.com>
2017-05-23 22:54:34 -0400

    Add support for walls and update scoreboard with mine count

--- public_html/BaseInvaders.js
+++ public_html/BaseInvaders.js
@@ -10,6 +10,56 @@ var canvas$;
 var g2x;
 var leaderboard$;
 
+var backgroundInitialized = false;
+
+function initializeBackground(context, state)
+{
+    var widthRatio = 1024.0 / state.width;
+    var heightRatio = 1024.0 / state.height;
+
+    context.clearRect(0, 0, state.width, state.height);
+
+    state.wormholes.forEach(function (wormhole) {
+        drawSpiral(context, wormhole.px * widthRatio, wormhole.py * heightRatio, wormhole.radius * widthRatio);
+    });
+
+    state.walls.forEach(function (wall) {
+        var x1 = wall.x1 * widthRatio;
+        var y1 = wall.y1 * heightRatio;
+        var x2 = wall.x2 * widthRatio;
+        var y2 = wall.y2 * heightRatio;
+        context.save();
+        context.beginPath();
+        context.strokeStyle = "#FFFFFF";
+        context.lineWidth=2;
+        context.moveTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.stroke();
+        context.closePath();
+        context.restore();
+    });
+
+    backgroundInitialized = true;
+}
+
+// Draws Archimedean spiral
+function drawSpiral(context, x, y, radius) {
+    context.save();
+    context.strokeStyle = "#0000FF";
+    context.lineWidth = 1.1;
+    context.beginPath();
+    context.moveTo(x, y);
+    tFactor = 0.1;
+    for (var i = 0; i < radius / tFactor; ++i) {
+        var angle = tFactor * i;
+        curX = x + (1 + angle) * Math.cos(angle);
+        curY = y + (1 + angle) * Math.sin(angle);
+        context.lineTo(curX, curY);
+        context.stroke();
+    }
+    context.closePath();
+    context.restore();
+}
 
 function repaint(init) {
     if (init) {
@@ -18,8 +68,16 @@ function repaint(init) {
         leaderboard$ = $("#leaderboard");
         canvas$.attr("width", 1024);
         canvas$.attr("height", 1024);
+
+        background$ = $("#gameCanvasBackground");
+        backgroundCtx = background$[0].getContext("2d");
+        background$.attr("width", 1024);
+        background$.attr("height", 1024);
     }
 
+    if (!backgroundInitialized) {
+        initializeBackground(backgroundCtx, state);
+    }
 
     var widthRatio = 1024.0 / state.width;
     var heightRatio = 1024.0 / state.height;
@@ -32,7 +90,7 @@ function repaint(init) {
     if (tableWidgets.length !== state.players.length) {
 
         leaderboard$.empty();
-        var table = $("<table style='width: 100%' ><th colspan='2' style='color: white; text-align: left'>Player</th><th style='color: white; text-align: right'>Score</th></table>");
+        var table = $(" <table style='width: 100%' > <th colspan='2' style='color: white; text-align: left'>Player</th> <th style='color: white; text-align: right'>Score</th> <th style='color: white; text-align: right'># Mines</th> </table>");
         leaderboard$.append(table);
 
         tableWidgets = [];
@@ -57,19 +115,25 @@ function repaint(init) {
             rowWidgets.push(img);
 
             table.append(tr);
-            td = $("<td style='text-align: left' />");
+            td = $("<td style='text-align: left; width: 150px' />");
             tr.append(td);
             var label = $("<label style='color:orange; font: monospace'/>");
             td.append(label);
             label.text(player.name);
             rowWidgets.push(label);
 
-
-            var td = $("<td style='text-align: right' />");
+            var td = $("<td style='text-align: right; width: 80px' />");
             tr.append(td);
             var label = $("<label style='color:orange; font: monospace'/>");
             td.append(label);
             label.text(player.score);
+            rowWidgets.push(label);
+
+            var td = $("<td style='text-align: right; width: 60px' />");
+            tr.append(td);
+            var label = $("<label style='color:orange; font: monospace'/>");
+            td.append(label);
+            label.text(player.minecount);
             rowWidgets.push(label);
 
             i++;
@@ -82,6 +146,7 @@ function repaint(init) {
             tableWidgets[i][0].attr("src", 'ship' + (tempPlayers[i].id % 60) + '.png');
             tableWidgets[i][1].text(tempPlayers[i].name);
             tableWidgets[i][2].text(tempPlayers[i].score);
+            tableWidgets[i][3].text(tempPlayers[i].minecount);
             pmap[state.players[i].name] = tempPlayers[i];
         }
     }
@@ -98,19 +163,17 @@ function repaint(init) {
         }
 
         g2x.beginPath();
-        g2x.arc(mine.px * widthRatio, mine.py * heightRatio, stroke * .4, 0, Math.PI * 2);
+        g2x.arc(mine.px * widthRatio, mine.py * heightRatio, stroke * .5, 0, Math.PI * 2);
         g2x.stroke();
     });
 
     state.bombs.forEach(function (bomb) {
         if (bomb.delay - 5 < bomb.life) {
-            console.log("pbig");
             g2x.fillStyle = "#FF0000";
             g2x.beginPath();
-            g2x.arc(bomb.px * widthRatio, bomb.py * heightRatio, stroke * state.bombRadius * Math.max(widthRatio, heightRatio), 0, Math.PI * 2);
+            g2x.arc(bomb.px * widthRatio, bomb.py * heightRatio, stroke * 3.6, 0, Math.PI * 2);
             g2x.fill();
         } else {
-            console.log("psmall", bomb.delay, bomb.life);
             g2x.fillStyle = "#FFFF00";
             g2x.beginPath();
             g2x.arc(bomb.px * widthRatio, bomb.py * heightRatio, stroke * .6, 0, Math.PI * 2);
@@ -119,26 +182,19 @@ function repaint(init) {
 
     });
 
-
     state.players.forEach(function (player) {
         g2x.translate(player.px * widthRatio, player.py * heightRatio);
         g2x.rotate(Math.atan2(player.vy, player.vx) + Math.PI / 2);
-        g2x.drawImage(shipImages[player.id % 60], -stroke * 1.2, -stroke * 1.2, stroke * 2.4, stroke * 2.4);
+        g2x.drawImage(shipImages[player.id % 60], -stroke * 1.2, -stroke * 1.2, stroke * 3.2, stroke * 2.8);
         g2x.setTransform(1, 0, 0, 1, 0, 0);
         g2x.fillStyle = "#FFFF00";
-        var radarSize = ((Date.now() / 15) + (player.id * 30)) % 500;
-        if (radarSize < 100) {
+        var maxRadarSize = state.visionRadius * widthRatio;
+        var radarSize = ((Date.now() / 20) + (player.id * (250 / state.players.length))) % (maxRadarSize * 5);
+        if (radarSize <= maxRadarSize) {
             g2x.beginPath();
             g2x.arc(player.px * widthRatio, player.py * widthRatio, radarSize, 0, Math.PI * 2);
             g2x.stroke();
         }
-    });
-
-    state.wormholes.forEach(function (wormhole) {
-        g2x.beginPath();
-        g2x.fillStyle = "#FFF000";
-        g2x.arc(wormhole.px * widthRatio, wormhole.py * heightRatio, wormhole.radius * widthRatio, 0, Math.PI * 2);
-        g2x.stroke();
     });
 
     function fill(inp, len, char) {
